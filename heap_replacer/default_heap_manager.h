61f422607b4986fd8d57ff3a1be7028d7b024797
iranrmrf <iranrmrf69@gmail.com>
2020-09-03 14:15:08 +0200

    Renamed mem_bank.h to default_heap.h and resized commit size

--- heap_replacer/default_heap_manager.h
+++ heap_replacer/default_heap_manager.h
@@ -2,12 +2,12 @@
 
 #include "list.h"
 #include "mem_cell.h"
-#include "mem_bank.h"
+#include "default_heap.h"
 
 #include "util.h"
 
 #define CELL_SIZE 4 * KB
-#define COMMIT_SIZE 64 * MB
+#define COMMIT_SIZE 32 * MB
 #define RESERVE_SIZE 256 * MB
 #define MAX_HEAP_SIZE 2 * GB
 
@@ -16,7 +16,7 @@ class default_heap_manager
 
 private:
 
-	mem_bank* bank;
+	default_heap* heap;
 
 	CRITICAL_SECTION critical_section;
 
@@ -24,7 +24,7 @@ public:
 
 	default_heap_manager()
 	{
-		this->bank = new mem_bank(CELL_SIZE, COMMIT_SIZE, RESERVE_SIZE, MAX_HEAP_SIZE);
+		this->heap = new default_heap(CELL_SIZE, COMMIT_SIZE, RESERVE_SIZE, MAX_HEAP_SIZE);
 
 		InitializeCriticalSectionAndSpinCount(&this->critical_section, INFINITE);
 	}
@@ -39,7 +39,7 @@ public:
 	void* malloc(size_t size)
 	{
 		EnterCriticalSection(&this->critical_section);
-		mem_cell* cell = this->bank->get_free_cell(size);
+		mem_cell* cell = this->heap->get_free_cell(size);
 		LeaveCriticalSection(&this->critical_section);
 		if (!cell) { return nullptr; }
 		return cell->desc.addr;
@@ -55,16 +55,16 @@ public:
 	size_t mem_size(void* address)
 	{
 		mem_cell* cell;
-		return (cell = this->bank->get_used(address)) ? cell->desc.size : 0;
+		return (cell = this->heap->get_used(address)) ? cell->desc.size : 0;
 	}
 
 	bool free(void* address)
 	{
 		EnterCriticalSection(&this->critical_section);
 		mem_cell* cell;
-		if (cell = this->bank->rmv_used(address))
+		if (cell = this->heap->rmv_used(address))
 		{
-			this->bank->add_free_cell(cell);
+			this->heap->add_free_cell(cell);
 		}
 		LeaveCriticalSection(&this->critical_section);
 		return true;
