2cd968592e4d74b1cf230a931aed517bb53f57cf
iranrmrf <iranrmrf69@gmail.com>
2020-09-20 00:40:41 +0200

    Renamed sh_array.h to sh_vector.h. Added CS macros.

--- heap_replacer/scrap_heap/sh_vector.h
+++ heap_replacer/scrap_heap/sh_vector.h
@@ -9,18 +9,18 @@ struct mt_sh
 	DWORD id;
 	scrap_heap* sh;
 
-	void* operator new(size_t size) { return nvhr_malloc(size); }
-	void operator delete(void* address) { nvhr_free(address); }
+	void* operator new(size_t size) { return NVHR::nvhr_malloc(size); }
+	void operator delete(void* address) { NVHR::nvhr_free(address); }
 };
 
-class sh_array
+class sh_vector
 {
 
 private:
 
-	mt_sh* data;
 	size_t size;
 	size_t alloc;
+	mt_sh* data;
 
 private:
 
@@ -28,56 +28,56 @@ private:
 
 public:
 
-	sh_array(size_t size)
+	sh_vector(size_t size) : size(0), alloc(size)
 	{
-		this->size = 0;
-		this->alloc = size;
-		this->data = (mt_sh*)nvhr_calloc(this->alloc, sizeof(mt_sh));
-
+		this->data = (mt_sh*)NVHR::nvhr_calloc(this->alloc, sizeof(mt_sh));
 		InitializeCriticalSectionEx(&this->critical_section, ~RTL_CRITICAL_SECTION_ALL_FLAG_BITS, RTL_CRITICAL_SECTION_FLAG_NO_DEBUG_INFO);
 	}
 
-	~sh_array()
+	~sh_vector()
 	{
-		nvhr_free(this->data);
+		NVHR::nvhr_free(this->data);
 		DeleteCriticalSection(&this->critical_section);
 	}
 
 	void insert(DWORD id, scrap_heap* sh)
 	{
-		EnterCriticalSection(&this->critical_section);
+		ECS(&this->critical_section);
 		if (this->size >= this->alloc)
 		{
 			this->alloc <<= 1;
-			mt_sh* temp = (mt_sh*)nvhr_realloc(this->data, this->alloc * sizeof(mt_sh));
+			mt_sh* temp = (mt_sh*)NVHR::nvhr_realloc(this->data, this->alloc * sizeof(mt_sh));
 			memmove(temp, this->data, this->size * sizeof(mt_sh));
-			nvhr_free(this->data);
+			NVHR::nvhr_free(this->data);
 			this->data = temp;
 		}
 		this->data[size++] = mt_sh { id, sh };
-		LeaveCriticalSection(&this->critical_section);
+		LCS(&this->critical_section);
 	}
 
 	scrap_heap* find(DWORD id)
 	{
+		ECS(&this->critical_section);
 		for (size_t i = 0; i < this->size; i++)
 		{
 			if (this->data[i].id == id)
 			{
+				LCS(&this->critical_section);
 				return this->data[i].sh;
 			}
 		}
+		LCS(&this->critical_section);
 		return nullptr;
 	}
 
 	void* operator new(size_t size)
 	{
-		return nvhr_malloc(size);
+		return NVHR::nvhr_malloc(size);
 	}
 
 	void operator delete(void* address)
 	{
-		nvhr_free(address);
+		NVHR::nvhr_free(address);
 	}
 
 };
