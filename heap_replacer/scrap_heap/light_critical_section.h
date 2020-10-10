782b11ac5526e804a0859128f5ecf23dec34985e
iranrmrf <iranrmrf69@gmail.com>
2020-10-10 21:47:50 +0200

    Neatened light_critical_section.

--- heap_replacer/scrap_heap/light_critical_section.h
+++ heap_replacer/scrap_heap/light_critical_section.h
@@ -2,34 +2,17 @@
 
 #include "main/util.h"
 
-struct light_critical_section
-{
-	DWORD thread_id;
-	size_t lock_count;
-};
+struct light_critical_section { DWORD thread_id; size_t lock_count; };
 
 void __fastcall enter_light_critical_section(TFPARAM(light_critical_section* self, const char* msg))
 {
 	DWORD id = GetCurrentThreadId();
-	if (self->thread_id == id)
-	{
-		++self->lock_count;
-	}
-	else
-	{
-		size_t spin = 0;
-		while (InterlockedCompareExchange(&self->thread_id, id, 0u))
-		{
-			Sleep(++spin > 10000u);
-		}
-		self->lock_count = 1;
-	}
+	if (self->thread_id == id) { self->lock_count++; }
+	else { while (InterlockedCompareExchange(&self->thread_id, id, NULL)) { } self->lock_count = 1; }
 }
 
 void __fastcall leave_light_critical_section(TFPARAM(light_critical_section* self))
 {
-	if (!--self->lock_count)
-	{
-		self->thread_id = 0;
-	}
+	--self->lock_count;
+	self->thread_id = BITWISE_IF_ELSE(!self->lock_count, NULL, self->thread_id);
 }
