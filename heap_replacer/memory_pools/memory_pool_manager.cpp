9532228a09ab2b35dc4c1fb571a07b48e4da147f
iranrmrf <iranrmrf69@gmail.com>
2021-05-04 18:16:59 +0200

    Created interface and updated to use interface

--- heap_replacer/memory_pools/memory_pool_manager.cpp
+++ heap_replacer/memory_pools/memory_pool_manager.cpp
@@ -2,8 +2,8 @@
 
 memory_pool_manager::memory_pool_manager()
 {
-	util::memset8(this->pools_by_size, 0u, pool_size_array_length * sizeof(memory_pool*));
-	util::memset8(this->pools_by_addr, 0u, pool_addr_array_length * sizeof(memory_pool*));
+	util::memset8(this->pools_by_size, 0u, pool_size_array_length * sizeof(memory_pool_interface*));
+	util::memset8(this->pools_by_addr, 0u, pool_addr_array_length * sizeof(memory_pool_interface*));
 	this->init_all_pools();
 
 #ifdef HR_USE_GUI
@@ -26,51 +26,49 @@ memory_pool_manager::~memory_pool_manager()
 
 void memory_pool_manager::init_all_pools()
 {
-	struct pool_data { size_t item_size; size_t max_size; } pool_desc[pool_count] =
+	memory_pool_interface* pools[pool_count] =
 	{
 #if defined(FNV)
-			{ 4u	, 0x01000000u },
-			{ 8u	, 0x04000000u },
-			{ 16u	, 0x04000000u },
-			{ 32u	, 0x08000000u },
-			{ 64u	, 0x04000000u },
-			{ 128u	, 0x10000000u },
-			{ 256u	, 0x08000000u },
-			{ 512u	, 0x04000000u },
-			{ 1024u	, 0x08000000u },
-			{ 2048u	, 0x08000000u },
+		new memory_pool<4u, 0x01000000u>(),
+		new memory_pool<8u, 0x04000000u>(),
+		new memory_pool<16u, 0x04000000u>(),
+		new memory_pool<32u, 0x08000000u>(),
+		new memory_pool<64u, 0x04000000u>(),
+		new memory_pool<128u, 0x10000000u>(),
+		new memory_pool<256u, 0x08000000u>(),
+		new memory_pool<512u, 0x04000000u>(),
+		new memory_pool<1024u, 0x08000000u>(),
+		new memory_pool<2048u, 0x08000000u>()
 #elif defined(FO3)
-			{ 4u	, 0x01000000u },
-			{ 8u	, 0x04000000u },
-			{ 16u	, 0x04000000u },
-			{ 32u	, 0x08000000u },
-			{ 64u	, 0x04000000u },
-			{ 128u	, 0x10000000u },
-			{ 256u	, 0x08000000u },
-			{ 512u	, 0x04000000u },
-			{ 1024u	, 0x08000000u },
-			{ 2048u	, 0x08000000u },
+		new memory_pool<4u, 0x01000000u>(),
+		new memory_pool<8u, 0x04000000u>(),
+		new memory_pool<16u, 0x04000000u>(),
+		new memory_pool<32u, 0x08000000u>(),
+		new memory_pool<64u, 0x04000000u>(),
+		new memory_pool<128u, 0x10000000u>(),
+		new memory_pool<256u, 0x08000000u>(),
+		new memory_pool<512u, 0x04000000u>(),
+		new memory_pool<1024u, 0x08000000u>(),
+		new memory_pool<2048u, 0x08000000u>()
 #endif
 	};
 	for (size_t i = 0u; i < pool_count; i++)
 	{
-		pool_data* pd = &pool_desc[i];
-		memory_pool* pool = new memory_pool(pd->item_size, pd->max_size);
-		void* address = pool->memory_pool_init();
-		this->pools_by_size[util::get_highest_bit(pd->item_size)] = pool;
-		for (size_t j = 0u; j < ((pd->max_size + (pool_alignment - 1u)) / pool_alignment); j++)
+		void* address = pools[i]->memory_pool_init();
+		this->pools_by_size[util::get_highest_bit(pools[i]->item_size())] = pools[i];
+		for (size_t j = 0u; j < ((pools[i]->max_size() + (pool_alignment - 1u)) / pool_alignment); j++)
 		{
-			this->pools_by_addr[((uintptr_t)address / pool_alignment) + j] = pool;
+			this->pools_by_addr[((uintptr_t)address / pool_alignment) + j] = pools[i];
 		}
 	}
 }
 
-memory_pool* memory_pool_manager::pool_from_size(size_t size)
+memory_pool_interface* memory_pool_manager::pool_from_size(size_t size)
 {
 	return this->pools_by_size[util::get_highest_bit(size)];
 }
 
-memory_pool* memory_pool_manager::pool_from_addr(void* address)
+memory_pool_interface* memory_pool_manager::pool_from_addr(void* address)
 {
 	return this->pools_by_addr[(uintptr_t)address / pool_alignment];
 }
@@ -103,14 +101,14 @@ void* memory_pool_manager::calloc(size_t size)
 
 size_t memory_pool_manager::mem_size(void* address)
 {
-	memory_pool* pool = this->pool_from_addr(address);
+	memory_pool_interface* pool = this->pool_from_addr(address);
 	if (!pool) [[unlikely]] { return 0u; }
 	return pool->mem_size(address);
 }
 
 bool memory_pool_manager::free(void* address)
 {
-	memory_pool* pool = this->pool_from_addr(address);
+	memory_pool_interface* pool = this->pool_from_addr(address);
 	if (!pool) [[unlikely]] { return false; }
 	pool->free(address);
 #ifdef HR_USE_GUI
