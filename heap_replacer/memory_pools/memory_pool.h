4a6136cf24fb5f08674afb880e14b8b645364465
iranrmrf <iranrmrf69@gmail.com>
2021-05-04 18:16:09 +0200

    Removed memory_pool.cpp to move to header

--- heap_replacer/memory_pools/memory_pool.h
+++ heap_replacer/memory_pools/memory_pool.h
@@ -2,9 +2,11 @@
 
 #include "main/util.h"
 
+#include "memory_pool_interface.h"
 #include "memory_pool_constants.h"
 
-class memory_pool
+template <size_t S, size_t M>
+class memory_pool : public memory_pool_interface
 {
 
 private:
@@ -13,8 +15,6 @@ private:
 
 private:
 
-	size_t item_size;
-	size_t max_size;
 	size_t cell_count;
 	size_t max_cell_count;
 
@@ -40,36 +40,141 @@ private:
 
 public:
 
-	memory_pool(size_t item_size, size_t max_size);
-	~memory_pool();
+	memory_pool() : cell_count(0u), pool_bgn(nullptr), pool_cur(nullptr), pool_end(nullptr)
+	{
+		this->block_count = M / pool_growth;
+		this->block_item_count = pool_growth / S;
 
-	void* memory_pool_init();
+		this->max_cell_count = this->block_count * this->block_item_count;
+
+		this->free_cells = (cell*)util::winapi_calloc(this->max_cell_count, sizeof(cell));
+		this->next_free = this->free_cells;
+
+		this->lock_id = 0u;
+	}
+
+	~memory_pool()
+	{
+		VirtualFree(this->free_cells, 0u, MEM_RELEASE);
+
+		VirtualFree(this->pool_bgn, 0u, MEM_RELEASE);
+	}
+
+	virtual void* memory_pool_init()
+	{
+		size_t i = 0x10u;
+		while (!this->pool_bgn)
+		{
+			this->pool_bgn = VirtualAlloc((void*)(i * pool_alignment), M, MEM_RESERVE, PAGE_READWRITE);
+			if (++i == 0xFFu) { i = 0x10u; }
+		}
+		this->pool_cur = this->pool_bgn;
+		this->pool_end = VPTRSUM(this->pool_bgn, M);
+		return this->pool_bgn;
+	}
 
 private:
 
-	void setup_new_block();
+	void setup_new_block()
+	{
+		this->pool_cur = VirtualAlloc(this->pool_cur, pool_growth, MEM_COMMIT, PAGE_READWRITE);
+		size_t bank_offset = UPTRDIFF(this->pool_cur, this->pool_bgn) / pool_growth * this->block_item_count;
+		this->free_cells[bank_offset].next = nullptr;
+		for (size_t i = 0u; i < this->block_item_count - 1u; i++)
+		{
+			this->free_cells[bank_offset + i + 1u].next = &this->free_cells[bank_offset + i];
+		}
+		this->next_free = &this->free_cells[bank_offset + this->block_item_count - 1u];
+		this->pool_cur = VPTRSUM(this->pool_cur, pool_growth);
+		this->cell_count += this->block_item_count;
+	}
 
-	void* free_ptr_to_real(void* address);
-	void* real_to_free_ptr(void* address);
+	void* free_ptr_to_real(void* address)
+	{
+		return VPTRSUM(this->pool_bgn, ((UPTRDIFF(address, this->free_cells) >> 2u) * S));
+	}
 
-	bool is_in_range(void* address);
+	void* real_to_free_ptr(void* address)
+	{
+		return VPTRSUM(((UPTRDIFF(address, this->pool_bgn) / S) << 2u), this->free_cells);
+	}
+
+	bool is_in_range(void* address)
+	{
+		return ((this->pool_bgn <= address) & (address < this->pool_end));
+	}
 
 public:
 
-	void* malloc();
-	void* calloc();
-	size_t mem_size(void* address);
-	void free(void* address);
+	virtual void* malloc()
+	{
+		LOCK(&this->lock_id);
+		if (!this->next_free->next) [[unlikely]]
+		{
+			if (this->pool_cur == this->pool_end) [[unlikely]]
+			{
+				UNLOCK(&this->lock_id);
+				return nullptr;
+			}
+			this->setup_new_block();
+		}
+		cell* old_free = this->next_free;
+		this->next_free = this->next_free->next;
+		this->cell_count--;
+		UNLOCK(&this->lock_id);
+		old_free->next = nullptr;
+		return this->free_ptr_to_real(old_free);
+	}
+
+	virtual void* calloc()
+	{
+		void* address = this->malloc();
+		if (address) [[likely]] { util::memset32(address, 0u, S >> 2u); }
+		return address;
+	}
+
+	virtual size_t mem_size(void* address)
+	{
+		return S;
+	}
+
+	virtual void free(void* address)
+	{
+		cell* c = (cell*)this->real_to_free_ptr(address);
+		LOCK(&this->lock_id);
+		if (!c->next) [[likely]]
+		{
+			this->cell_count++;
+			c->next = this->next_free;
+			this->next_free = c;
+	#ifdef HR_ZERO_MEM
+			util::memset32(address, 0u, this->item_size >> 2u);
+	#endif
+		}
+		UNLOCK(&this->lock_id);
+	}
 	
+public:
+
+	virtual size_t item_size() { return S; }
+	virtual size_t max_size() { return M; }
+
 public:
 
 	size_t get_cell_count() { return this->cell_count; }
 	size_t get_max_cell_count() { return this->max_cell_count; }
-	size_t get_max_size() { return this->max_size; }
+	size_t get_max_size() { return M; }
 
 public:
 
-	void* operator new(size_t size);
-	void operator delete(void* address);
+	void* operator new(size_t size)
+	{
+		return hr::hr_ina_malloc(size);
+	}
+
+	void operator delete(void* address)
+	{
+		hr::hr_ina_free(address);
+	}
 
 };
